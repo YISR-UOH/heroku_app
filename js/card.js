8bd9a20d56407fe77a1fc2afb592eb9ceeed87ca
YISR-UOH <ysepu098@gmail.com>
2021-05-28 16:13:09 -0400

    git create
    se agrego el avance realizado en local para trabajar en grupo desde este punto.

---
+++ js/card.js
@@ -0,0 +1,31 @@
+function changeImageSrc(x){
+    document.getElementById(x[0]).src = x[1];
+}
+
+function changePrice(x){
+    document.getElementById(x[0]).innerHTML = x[1];
+}
+
+
+
+
+const select = document.querySelector('#select');
+const opciones = document.querySelector('#opciones');
+const contenidoSelect = document.querySelector('#select .contenido-select');
+const hiddenInput = document.querySelector('#inputSelect');
+
+document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
+	opcion.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenidoSelect.innerHTML = e.currentTarget.innerHTML;
+		select.classList.toggle('active');
+		opciones.classList.toggle('active');
+		hiddenInput.value = e.currentTarget.querySelector('.titulo').innerText;
+	});
+});
+
+select.addEventListener('click', () => {
+	select.classList.toggle('active');
+	opciones.classList.toggle('active');
+});
+

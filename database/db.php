46019d3e1fb969e7aac02c267490877b40368355
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2021-06-13 23:51:27 -0400

    Delete database directory

--- database/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $mysqli = new mysqli('bojyzjbmwzrwp95xw2by-mysql.services.clever-cloud.com:3306',  'urx7akvfarhudpec', 'd6E42LtkzyEX5HaYSXue','bojyzjbmwzrwp95xw2by');
-    $mysqli->set_charset("utf8");
-    if  (!$mysqli) {
-        die('No pudo conectarse: ' . mysql_error());
-    }
-?>

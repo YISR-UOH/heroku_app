9545fb1620cdbb4ac1e4f37a8e3a3fe0baf89526
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-01 14:25:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,20 +11,20 @@
 
         <p> hola aaa<p>
         <?php
-        
-
-
-            $serverName = "com501.database.windows.net"; // update me
-            $connectionOptions = array(
-                "Database" => "test", // update me
-                "Uid" => "YISR", // update me
-                "PWD" => "yerkoISR098" // update me
-            );
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-
+        $serverName = "com501.database.windows.net"; // update me
+        $connectionOptions = array(
+            "Database" => "test", // update me
+            "Uid" => "YISR", // update me
+            "PWD" => "yerkoISR098" // update me
+        );
+        try{
+            $conn = sqlsrv_connect($serverName, $connectionOptions);
+        }
+        catch{
             echo "<p> adasdaf <p>";
-            $sql = "SELECT name FROM SYSOBJECTS WHERE xtype = 'U'";
-            echo "<p>". $sql ."<p>";
+        }
+        $sql = "SELECT name FROM SYSOBJECTS WHERE xtype = 'U'";
+        echo "<p>". $sql ."<p>";
         
         ?>
     </body>

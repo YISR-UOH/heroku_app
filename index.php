957e63810583e6f9710ccd64d85d736141ca767f
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-01 13:56:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             if ($result->num_rows > 0) {
             // output data of each row
                 while($row = $result->fetch_assoc()) {
-                    echo "id: " . $row["name"] "<br>";
+                    echo "id: " . $row["name"]. "<br>";
                 }
             } else {
               echo "0 results";

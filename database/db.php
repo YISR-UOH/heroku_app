1481a21ddc7873287dad3993fea6a1b69eee3f01
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-01 13:29:40 -0400

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,19 +1,13 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url("mysql://bd3165cf47e064:a65cb28a@us-cdbr-east-05.cleardb.net/heroku_3a879034930740d?reconnect=true");
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
+// PHP Data Objects(PDO) Sample Code:
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:com501.database.windows.net,1433; Database = test", "YISR", "yerkoISR098");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
+
 ?>

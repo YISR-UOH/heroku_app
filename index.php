dc0615eb456b94468d5ac909e9bdce5b7fd06cbd
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-06 15:01:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,23 @@
 
         <p> hola aaa<p>
         <?php
+            $query = 'SELECT * FROM test LIMIT 100';
+            $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
 
-        
+            echo "<table>\n";
+            while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                echo "\t<tr>\n";
+                foreach ($line as $col_value) {
+                    echo "\t\t<td>$col_value</td>\n";
+                }
+                echo "\t</tr>\n";
+            }
+            echo "</table>\n";
+            // Liberando el conjunto de resultados
+            pg_free_result($result);
+
+            // Cerrando la conexión
+            pg_close($conn);
         ?>
     </body>
 </html>

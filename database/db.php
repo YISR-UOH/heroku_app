8745c0acee1e6ed7c23cbfd00e4935807fc9e90c
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-06 15:37:28 -0400

    Update db.php

--- database/db.php
+++ database/db.php
@@ -2,15 +2,5 @@
 
 // Create connection
 
-$conn = new PDO('pgsql:host=bcgvo3crlk65uicfag0j-postgresql.services.clever-cloud.com;dbname=test', 'uw49pfruyeq3om3uq0tk', 'uw49pfruyeq3om3uq0tk');
-
-// Check connection
-echo "hola mundo";
-
-if ($conn) {
-    echo "Successfully connected to database: " . pg_dbname($conn) . " on " .  pg_host($conn) . "<br/>\n";
-} else {
-    echo pg_last_error($conn);
-    exit;
-}
+$conn = pg_pconnect("host=bcgvo3crlk65uicfag0j-postgresql.services.clever-cloud.com port=5432 dbname=test user=uw49pfruyeq3om3uq0tk password=uw49pfruyeq3om3uq0tk");
 ?>

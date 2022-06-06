4c3496ca28e1553b8a54bb0608102f3ae198819b
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-06 15:06:22 -0400

    Update db.php

--- database/db.php
+++ database/db.php
@@ -8,7 +8,7 @@ $port = 5432;
 $dbname = "test";
 
 // Create connection
-$conn = pg_connect("host=bcgvo3crlk65uicfag0j-postgresql.services.clever-cloud.com port=5432 dbname=mary user=uw49pfruyeq3om3uq0tk password=uw49pfruyeq3om3uq0tk");
+$conn = pg_connect("host=bcgvo3crlk65uicfag0j-postgresql.services.clever-cloud.com port=5432 dbname=test user=uw49pfruyeq3om3uq0tk password=uw49pfruyeq3om3uq0tk");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);

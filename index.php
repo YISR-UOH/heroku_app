f1fa5bf4b5106326b6844d5c636fa4ab18109391
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-06 15:17:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <p> hola aaa<p>
         <?php
             include 'database/db.php';
-            echo pg_dbname();
+            echo "<p> asda -" .pg_dbname(). "--<p>";
             $result = pg_query($conn, "SELECT * FROM test");
             if (!$result) {
               echo "An error occurred.\n";

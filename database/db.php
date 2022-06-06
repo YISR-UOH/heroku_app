38a495de0e9a625c1f7615735d79a8cb71fd2bdb
YISR-UOH <52255621+YISR-UOH@users.noreply.github.com>
2022-06-06 15:12:27 -0400

    Update db.php

--- database/db.php
+++ database/db.php
@@ -13,30 +13,5 @@ $conn = pg_connect("host=bcgvo3crlk65uicfag0j-postgresql.services.clever-cloud.c
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-else{
-  echo '<p>funciona<p>';
-}
-
-function get_tables() {
-  $tables = array();
-	$names = db_list("
-		SELECT
-			array_to_string( array_agg(c.relname), ',' ) AS names
-		FROM
-			pg_catalog.pg_class c
-		LEFT JOIN
-			pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-		WHERE
-			c.relkind IN ('r', '')
-		AND
-			n.nspname NOT IN ('pg_catalog', 'pg_toast')
-		AND
-			pg_catalog.pg_table_is_visible(c.oid)
-	", ',', false);
-	foreach($names as $name){
-		$tables[$name] = db_table_structure($name);
-	}
-	return $tables;
-}
 
 ?>
